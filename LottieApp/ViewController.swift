5bd8300fa0f1e5f2f221e0c51ea081620a046a67
AlexeyRossosh <alexey.rosshansky@rentateam.ru>
2018-11-07 14:04:02 +0300

    Initial Commit

---
+++ LottieApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LottieApp
+//
+//  Created by Alexey Rossoshansky on 07/11/2018.
+//  Copyright © 2018 Alexey Rossoshansky. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
